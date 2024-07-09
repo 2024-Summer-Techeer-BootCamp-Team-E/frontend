ea5773f3b6822498a3ffc8de9ce63d29654f2e0e
Linseo-ID <yeonkyulee4@gmail.com>
2024-07-09 23:22:47 +0900

    design: 메인 온보딩 페이지 퍼블리싱

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,7 @@ module.exports = {
         mainBg: '#F8F6F4',
         orange: '#FF7425',
         footer: '#001E42',
-      },
-      backgroundImage: {
-        'gradient-text': 'linear-gradient(90deg, #F49864 0%, #FFA06B 19.29%, #FF9A61 32.88%, #FF7629 47.1%, #FF5C00 100%)',
+        lightorange: '#F3A261',
       },
     },
   },
