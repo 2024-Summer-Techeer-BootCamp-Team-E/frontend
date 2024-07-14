e5d017a21e76472dc40ce71979dba71d5f38f150
Linseo-ID <yeonkyulee4@gmail.com>
2024-07-14 16:36:35 +0900

    feat: 회원가입 API 연동

--- src/components/User/SignupModal.tsx
+++ src/components/User/SignupModal.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 interface SignupModalProps {
   closeSignupModal: () => void
@@ -6,21 +8,67 @@ interface SignupModalProps {
 }
 
 const SignupModal = ({ closeSignupModal, openLoginModal }: SignupModalProps) => {
+  // const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [repeatPassword, setRepeatPassword] = useState('')
+  const name = 'lyk'
+  const [user_id, setUser_id] = useState('')
+  // const [name, setName] = useState('')
+
+  const handleSignup = async () => {
+    if (password !== repeatPassword) {
+      alert('비밀번호가 일치하지 않습니다.')
+      return
+    }
+
+    try {
+      const response = await axios.post('http://localhost:8000/api/v1/accounts/register/', {
+        user_id,
+        password,
+        name,
+      })
+      console.log(response.data)
+    } catch (error) {
+      console.log('Error', error)
+    }
+  }
+
   return (
     <div className="fixed top-0 z-20 flex items-center justify-center w-screen h-screen snap-none bg-black/30 backdrop-blur-[2px]">
       <div className="rounded-2xl shadow-lg w-[390px] h-[350px] bg-white px-9">
         <div className="flex items-center justify-between">
           <p className="mt-4 text-xl font-semibold">회원가입</p>
-          <p className="text-lg cursor-pointer" onClick={closeSignupModal}>
+          <p className="mt-4 text-lg cursor-pointer" onClick={closeSignupModal}>
             X
           </p>
         </div>
-        <input className="pl-5 mt-5 text-lg text-black bg-white border rounded-lg outline-none w-80 h-11" placeholder="example@email.com" />
-        <input className="pl-5 mt-5 text-lg text-black bg-white border rounded-lg outline-none w-80 h-11" placeholder="Password" type="password" />
-        <input className="pl-5 mt-5 text-lg text-black bg-white border rounded-lg outline-none w-80 h-11" placeholder="Repeat Password" type="password" />
-        <button className="mt-5 text-xl font-bold text-white border rounded-lg shadow-lg w-80 h-11 bg-hongsi">가입하기</button>
-        <div className="flex items-center justify-end mt-3">
-          <p className="text-sm cursor-pointer text-hongsi hover:underline" onClick={openLoginModal}>
+        <input
+          className="pl-5 mt-5 text-lg text-black bg-white border rounded-lg outline-none w-80 h-11"
+          placeholder="example@email.com"
+          value={user_id}
+          onChange={(e) => setUser_id(e.target.value)}
+        />
+        <input
+          className="pl-5 mt-5 text-lg text-black bg-white border rounded-lg outline-none w-80 h-11"
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <input
+          className="pl-5 mt-5 text-lg text-black bg-white border rounded-lg outline-none w-80 h-11"
+          placeholder="Repeat Password"
+          type="password"
+          value={repeatPassword}
+          onChange={(e) => setRepeatPassword(e.target.value)}
+        />
+
+        <button className="mt-5 text-xl font-bold text-white border rounded-lg shadow-lg w-80 h-11 bg-hongsi" onClick={handleSignup}>
+          가입하기
+        </button>
+        <div className="flex items-center justify-end ">
+          <p className="mt-3 text-sm cursor-pointer text-hongsi hover:underline" onClick={openLoginModal}>
             로그인
           </p>
         </div>
