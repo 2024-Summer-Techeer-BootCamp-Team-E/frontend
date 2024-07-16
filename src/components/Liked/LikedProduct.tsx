a5674a242d0ae208ae072e234745a34b787734bb
Yeongjun Joo <wndudwns6824@naver.com>
2024-07-16 20:48:34 +0900

    feat : delete 버튼 수정

--- src/components/Liked/LikedProduct.tsx
+++ src/components/Liked/LikedProduct.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
-import Liked from '../../assets/images/Liked.png'
 import useLikeP from '../../store/useLikeP'
+import Like from '../../assets/images/Like.png'
+import Liked from '../../assets/images/Liked.png'
+import { useState } from 'react'
 
 interface LikedProducts {
   id: number
@@ -12,41 +14,53 @@ interface LikedProducts {
   category_id: number
 }
 
-export default function LikedProduct({ id, name, price, link, delivery_charge, image_url }: LikedProducts) {
-  const { updateLikeState } = useLikeP()
+export default function LikedProduct({ id, name, price, link, delivery_charge, image_url, category_id }: LikedProducts) {
+  const [like, setLike] = useState(true)
+
+  const TOKEN =
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzIxMTUwODIwLCJpYXQiOjE3MjExMjkyMjAsImp0aSI6IjdiMjVjODg0ZmM2MTQyYWRhMTA4NjU5OGVhYWQ5ZmZkIiwidXNlcl9pZCI6MX0.y3jMCTlaQL3ycSNTkipvbsnCBd7VnWzP2oRS86Fcbz4'
   const HandleClickLink = () => {
     window.open(link, '_blank')
   }
-
-  const TOKEN =
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzIxMTI0OTMzLCJpYXQiOjE3MjExMDMzMzMsImp0aSI6IjEyODIyOTA1N2NlODQ5OGJhNGNmMTkyMmE5YTlkNjM3IiwidXNlcl9pZCI6MX0.6rw4-4u0pMoNtjjFjqfSealI3fLi5Dr4xMUOAFCaWhk'
-  const GetLikes = async () => {
+  const HandleClickLike = async () => {
     try {
-      const response = await axios.get('http://localhost:8000/api/v1/likes', {
-        headers: {
-          accept: 'application/json',
-          Authorization: TOKEN,
+      setLike(!like)
+      const response = await axios.post(
+        'http://localhost:8000/api/v1/likes/',
+        {
+          name,
+          price,
+          delivery_charge,
+          link,
+          image_url,
+          category_id,
         },
-      })
+        {
+          headers: {
+            accept: 'application/json',
+            Authorization: TOKEN,
+          },
+        },
+      )
       console.log(response.data)
-      updateLikeState(response.data)
-      console.log('Get 성공')
+      console.log('성공')
+      alert('POST 성공!')
     } catch (error) {
       console.log('실패')
     }
   }
+
   const HandleClickUnLike = async () => {
     try {
+      setLike(!like)
       const response = await axios.delete('http://localhost:8000/api/v1/likes', {
         headers: {
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzIxMTI0OTMzLCJpYXQiOjE3MjExMDMzMzMsImp0aSI6IjEyODIyOTA1N2NlODQ5OGJhNGNmMTkyMmE5YTlkNjM3IiwidXNlcl9pZCI6MX0.6rw4-4u0pMoNtjjFjqfSealI3fLi5Dr4xMUOAFCaWhk',
+          Authorization: TOKEN,
         },
         data: { id },
       })
       console.log(response.data)
       alert('DELETE 성공!')
-      GetLikes()
     } catch (error) {
       console.log('실패')
     }
@@ -66,9 +80,7 @@ export default function LikedProduct({ id, name, price, link, delivery_charge, i
 
           <p className=" lg:w-28 md:w-20 sm:w-16 text-hongsi">₩{price}</p>
         </div>
-        <div className="w-8 h-8 my-2">
-          <img src={Liked} alt="Liked" className="" onClick={HandleClickUnLike} />
-        </div>
+        <div className="w-8 h-8 my-2">{like ? <img src={Liked} alt="Liked" onClick={HandleClickUnLike} /> : <img src={Like} alt="Liked" onClick={HandleClickLike} />}</div>
       </div>
     </div>
   )
