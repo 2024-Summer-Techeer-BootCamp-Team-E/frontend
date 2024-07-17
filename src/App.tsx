029dfb5bfaead98ed7f6231d8c80eee7ae1f20f0
Yeongjun Joo <wndudwns6824@naver.com>
2024-07-18 01:16:04 +0900

    feat : faker.js로 랜덤 데이터 post 요청 및 react-query로 좋아요 api 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import MainPage from './pages/MainPage'
 import LikedPage from './pages/LikedPage'
 import SearchResPage from './pages/SearchResPage'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/searchres" element={<SearchResPage />} />
-        <Route path="/liked" element={<LikedPage />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/searchres" element={<SearchResPage />} />
+          <Route path="/liked" element={<LikedPage />} />
+        </Routes>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 export default App
--- src/components/Liked/LikedProduct.tsx
+++ src/components/Liked/LikedProduct.tsx
@@ -1,8 +1,9 @@
-import axios from 'axios'
-import useLikeP from '../../store/useLikeP'
+import axios, { AxiosResponse } from 'axios'
 import Like from '../../assets/images/Like.png'
 import Liked from '../../assets/images/Liked.png'
 import { useState } from 'react'
+import { faker } from '@faker-js/faker'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 interface LikedProducts {
   id: number
@@ -13,87 +14,110 @@ interface LikedProducts {
   image_url: string
   category_id: number
 }
-
+const postLike = async (): Promise<AxiosResponse<LikedProducts>> => {
+  const token = localStorage.getItem('accessToken')
+  return axios.post(
+    'http://localhost:8000/api/v1/likes/',
+    {
+      name: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      delivery_charge: faker.commerce.price(),
+      link: faker.internet.url(),
+      image_url: faker.image.url(),
+      category_id: 7,
+    },
+    {
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  )
+}
 export default function LikedProduct({ id, name, price, link, delivery_charge, image_url, category_id }: LikedProducts) {
+  const queryClient = useQueryClient()
   const [like, setLike] = useState(true)
-  const { likeP, updateLikeState } = useLikeP()
-  const HandleClickLink = () => {
+  const token = localStorage.getItem('accessToken')
+
+  const handleClickLink = () => {
     window.open(link, '_blank')
   }
-  const token = localStorage.getItem('accessToken')
-  // const HandleClickLike = async () => {
-  //   try {
-  //     setLike(!like)
-  //     const response = await axios.post(
-  //       'http://localhost:8000/api/v1/likes/',
-  //       {
-  //         name,
-  //         price,
-  //         delivery_charge,
-  //         link,
-  //         image_url,
-  //         category_id,
-  //       },
-  //       {
-  //         headers: {
-  //           accept: 'application/json',
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //       },
-  //     )
-  //     console.log(response.data)
-  //     console.log(likeP)
-  //     console.log('성공')
-  //     alert('POST 성공!')
-  //   } catch (error) {
-  //     console.log('실패')
-  //   }
-  // }
 
-  const HandleClickUnLike = async () => {
-    try {
-      setLike(!like)
-      const response = await axios.delete('http://localhost:8000/api/v1/likes', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        data: { id },
-      })
-      alert('좋아요 취소!')
-    } catch (error) {
-      console.log('실패')
-    }
-    try {
-      const response = await axios.get('http://localhost:8000/api/v1/likes', {
+  useQuery({
+    queryKey: ['product'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:8000/api/v1/likes', {
         headers: {
           accept: 'application/json',
           Authorization: `Bearer ${token}`,
         },
       })
-      console.log(response.data)
-      updateLikeState(response.data)
-      console.log('Get 성공')
-    } catch (error) {
-      console.log('실패')
-    }
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      return response.json()
+    },
+  })
+  const postMutation = useMutation<AxiosResponse<LikedProducts>, Error, void>({
+    mutationFn: postLike,
+    onSuccess: () => {
+      console.log('좋아요 성공')
+      // 요청 성공 시 해당 queryKey 유효성 제거
+      queryClient.invalidateQueries({
+        queryKey: ['product'],
+      })
+    },
+    onError: () => {
+      console.error('에러 발생')
+    },
+  })
+  const deleteLike = async (): Promise<AxiosResponse<LikedProducts>> => {
+    return axios.delete('http://localhost:8000/api/v1/likes/', {
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      data: { id },
+    })
   }
+
+  const deleteMutation = useMutation<AxiosResponse<LikedProducts>, Error, void>({
+    mutationFn: deleteLike,
+    onSuccess: () => {
+      console.log('삭제 성공')
+      queryClient.invalidateQueries({
+        queryKey: ['product'],
+      })
+      alert('좋아요 취소!')
+    },
+    onError: () => {
+      console.error('에러 발생')
+    },
+  })
+  const handlePostDate = () => {
+    setLike(false)
+    postMutation.mutate()
+  }
+  const handleDeleteDate = () => {
+    setLike(false)
+    deleteMutation.mutate()
+  }
+
   return (
     <div>
       <div className="w-full mt-2 border-t-4 border-mainBg" />
       <div className="flex justify-between my-4 text-sm sm:justify-between">
-        <img src={image_url} alt="image" className="cursor-pointer w-28 h-28" onClick={HandleClickLink} />
+        <img src={image_url} alt="image" className="cursor-pointer w-28 h-28" onClick={handleClickLink} />
         <div className="flex flex-col my-4 text-sm sm:justify-between sm:flex-row">
-          <p className="font-semibold cursor-pointer xl:w-[410px] lg:w-[410px] md:w-64 w-48" onClick={HandleClickLink}>
-            {id}
+          <p className="font-semibold cursor-pointer xl:w-[410px] lg:w-[410px] md:w-64 w-48" onClick={handleClickLink}>
             {name}
           </p>
           <p className="lg:w-40 md:w-28 sm:w-20 text-black/50">
             <span className="sm:hidden">배송비: </span> ₩{delivery_charge}
           </p>
-
-          <p className=" lg:w-28 md:w-20 sm:w-16 text-hongsi">₩{price}</p>
+          <p className="lg:w-28 md:w-20 sm:w-16 text-hongsi">₩{price}</p>
         </div>
-        <div className="w-8 h-8 my-2">{like ? <img src={Liked} alt="Liked" onClick={HandleClickUnLike} /> : <img src={Like} alt="Liked" />}</div>
+        <div className="w-8 h-8 my-2">{like ? <img src={Liked} alt="Liked" onClick={handleDeleteDate} /> : <img src={Like} alt="Like" onClick={handlePostDate} />}</div>
       </div>
     </div>
   )
