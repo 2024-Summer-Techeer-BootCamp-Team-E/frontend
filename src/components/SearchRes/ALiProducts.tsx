606df739b35905a2d8fd5fdecca24db102ed33b0
Yeongjun Joo <wndudwns6824@naver.com>
2024-07-23 02:20:20 +0900

    feat : react-query로 좋아요 기능 구현

--- src/components/SearchRes/ALiProducts.tsx
+++ src/components/SearchRes/ALiProducts.tsx
@@ -1,6 +1,8 @@
-import Liked from '../../assets/images/Liked.png'
 import Like from '../../assets/images/Like.png'
+import Liked from '../../assets/images/Liked.png'
 import { useState } from 'react'
+import axios, { AxiosResponse } from 'axios'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 interface ALiState {
   id: number
@@ -14,12 +16,87 @@ interface ALiState {
 
 export default function ALiProducts({ product_name, price, delivery_charge, link, image_url, category_id }: ALiState) {
   const [like, setLike] = useState(false)
-  const handleClickLike = () => {
+  const queryClient = useQueryClient()
+  const [pData, setPData] = useState<ALiState | null>(null)
+  const token = localStorage.getItem('accessToken')
+  const postData = async () => {
+    const response = await axios.post<ALiState>(
+      '/api/v1/likes/',
+      {
+        name: product_name,
+        price,
+        delivery_charge,
+        link,
+        image_url,
+        category_id,
+      },
+      {
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      },
+    )
+    return response.data
+  }
+
+  const deleteLike = async (): Promise<AxiosResponse<ALiState>> => {
+    if (!pData || !pData.id) {
+      throw new Error('pData는 null이 아니다.')
+    }
+    return axios.delete('http://localhost:8000/api/v1/likes', {
+      data: { id: pData.id },
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+  }
+
+  const postMutation = useMutation<ALiState, Error, void>({
+    mutationFn: postData,
+    onSuccess: (data) => {
+      console.log('좋아요 성공')
+      queryClient.invalidateQueries({
+        queryKey: ['product'],
+      })
+      setPData(data)
+      alert('좋아요 성공!')
+    },
+    onError: () => {
+      console.error('에러 발생')
+    },
+  })
+
+  const deleteMutation = useMutation<AxiosResponse<ALiState>, Error, void>({
+    mutationFn: deleteLike,
+    onSuccess: () => {
+      console.log('삭제 성공')
+      queryClient.invalidateQueries({
+        queryKey: ['product'],
+      })
+
+      alert('좋아요 취소!')
+    },
+    onError: () => {
+      console.error('에러 발생')
+    },
+  })
+
+  const handleClickPostDelete = () => {
+    if (like) {
+      deleteMutation.mutate()
+    } else {
+      postMutation.mutate()
+    }
     setLike(!like)
   }
+
   const handleClickLink = () => {
     window.open(link, '_blank')
   }
+
   return (
     <div className="flex flex-col items-center justify-between gap-2 my-3 font-semibold xl:w-60 lg:w-[216px] md:w-56 w-full">
       <div className="relative flex flex-col items-center">
@@ -27,11 +104,10 @@ export default function ALiProducts({ product_name, price, delivery_charge, link
         <p className="text-xs sm:text-sm lg:text-base">{product_name}</p>
         <div className="absolute right-2 top-2 ">
           {like ? (
-            <img src={Liked} alt="liked" onClick={handleClickLike} className="w-6 h-6 sm:w-8 sm:h-8" />
+            <img src={Liked} alt="liked" onClick={handleClickPostDelete} className="w-6 h-6 sm:w-8 sm:h-8" />
           ) : (
-            <img src={Like} alt="like" onClick={handleClickLike} className="w-6 h-6 sm:w-8 sm:h-8" />
+            <img src={Like} alt="like" onClick={handleClickPostDelete} className="w-6 h-6 sm:w-8 sm:h-8" />
           )}
-          {/* 좋아요 누를때 효과나 알림 주면 좋을 듯 */}
         </div>
       </div>
       <div className="w-full">
